d91b851ff60f606f001d210463c614a9193da293
langrenhualiwan-dagou <80331841+langrenhualiwan-dagou@users.noreply.github.com>
2021-03-26 23:31:26 +0800

    输出小于N之内的数要求能同时被3和7的数整除然后计算这些数的平方根

---
+++ 和的平方根.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<math.h>
+double fun(int n);
+int main(){
+	double k;
+	int n;
+	scanf("%d", &n);
+	k = fun(n);
+	printf("%lf\n", k);
+    return 0;
+}
+double fun(int n){
+	int sum = 0;
+	for (int i = 21; i < n; i++){
+		if (i % 3 == 0 && i% 7 == 0){
+			sum = sum + i;
+		}
+	}
+		return sqrt(sum); 
+
+}*/
